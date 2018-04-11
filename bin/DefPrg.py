6c62dcf38106849ad9a9e0184bc08122e3a41e7d
Ursus Arctos Horribilis <uibbs2@gmail.com>
2018-04-11 18:51:52 +0200

    newprg and defprg modified

--- bin/DefPrg.py
+++ bin/DefPrg.py
@@ -1,14 +1,17 @@
 programma = ( ##### PROGRAMMAZIONE RELE
 ##### SINTASSI:
 ## Ogni riga nella sintassi OBBLIGATORIA:
-## <tab>(rele,pausa),
+## <tab>(rele[,rele,rele], pausa),
 ## Usare "#" per i commenti
 ## Attiva il rele, poi aspetta per pausa; usare 0 per saltare la pausa
 ## Cominciare il programma con (0,pausa), per forzare un'attesa iniziale
 ##
 ######################################################################
 
-	(0,2),
+	1,		# (MANDATORY) Number of repetitions: 1-X or -1 for infinite loop
+
+	(0,2), # Port 0: won't operate any relais, will just wait for X seconds
+
 	(16,1),
 	(12,3),
 	(12,2),
